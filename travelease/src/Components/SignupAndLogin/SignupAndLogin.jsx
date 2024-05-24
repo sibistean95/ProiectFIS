cf79275974cb34c8a4baf9c2d91c32d05cbe0cff
Claudiu Popa-Panda <claudiu.popa-panda@student.upt.ro>
2024-05-25 02:41:54 +0300

    Frontend : Login : Basic page with icons

--- travelease/src/Components/SignupAndLogin/SignupAndLogin.jsx
+++ travelease/src/Components/SignupAndLogin/SignupAndLogin.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './SignupAndLogin.css';
 
+import { FaUser, FaLock } from "react-icons/fa";
+
 const SignupAndLogin = () => {
     return (
         <div className='wrapper'>
@@ -10,10 +12,12 @@ const SignupAndLogin = () => {
                     <div className="input-box">
                         <input type="text"
                         placeholder="Username" required />
+                        <FaUser className="icon"/>
                     </div>
                     <div className="input-box">
                         <input type="password"
                         placeholder="Password" required />
+                        <FaLock className="icon"/>
                     </div>
 
                     <div className="remember-forgot">
