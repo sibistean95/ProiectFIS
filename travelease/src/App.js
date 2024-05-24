1cfaf6b21a062b1029e7cbf870cab39431217221
Claudiu Popa-Panda <claudiu.popa-panda@student.upt.ro>
2024-05-25 02:02:12 +0300

    Frontend : Signup and Login : Introducing .css file

--- travelease/src/App.js
+++ travelease/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SignupAndLogin from './Components/SignupAndLogin/SignupAndLogin';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <SignupAndLogin />
     </div>
   );
 }
--- travelease/src/Components/SignupAndLogin/SignupAndLogin.jsx
+++ travelease/src/Components/SignupAndLogin/SignupAndLogin.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './SignupAndLogin.css';
 
 const SignupAndLogin = () => {
     return (
         <div>SignupAndLogin</div>
-    )
-}
+    );
+};
 
 export default SignupAndLogin
