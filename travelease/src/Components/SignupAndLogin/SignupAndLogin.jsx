d8e6a9cf39b232ad8e6a79736f1b2bfa980e616d
Claudiu Popa-Panda <claudiu.popa-panda@student.upt.ro>
2024-05-25 02:32:24 +0300

    Frontend : Signup and Login : Introducing .css file

--- travelease/src/Components/SignupAndLogin/SignupAndLogin.jsx
+++ travelease/src/Components/SignupAndLogin/SignupAndLogin.jsx
@@ -3,8 +3,40 @@ import './SignupAndLogin.css';
 
 const SignupAndLogin = () => {
     return (
-        <div>SignupAndLogin</div>
+        <div className='wrapper'>
+            <div className="form-box login">
+                <form action="">
+                    <h1>Login</h1>
+                    <div className="input-box">
+                        <input type="text"
+                        placeholder="Username" required />
+                    </div>
+                    <div className="input-box">
+                        <input type="password"
+                        placeholder="Password" required />
+                    </div>
+
+                    <div className="remember-forgot">
+                        <label>
+                            <input type="checkbox" /> Remember me
+                        </label>
+                        <a href="#">Forgot password ?</a>
+                    </div>
+
+                    <button type="submit">Login</button>
+
+                    <div className="register-link">
+                        <p>
+                            Don't have an account ?
+                            <a href="#">Register</a>
+                        </p>
+                    </div>
+
+                </form>
+            </div>
+        </div>
     );
 };
+
 
 export default SignupAndLogin
