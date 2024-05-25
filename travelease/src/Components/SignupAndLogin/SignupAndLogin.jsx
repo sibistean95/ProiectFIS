f8d52d6564b55317f4ba00109954b8ec234267c7
Claudiu Popa-Panda <claudiu.popa-panda@student.upt.ro>
2024-05-25 06:01:39 +0300

    Frontend : Signup : Implementation

--- travelease/src/Components/SignupAndLogin/SignupAndLogin.jsx
+++ travelease/src/Components/SignupAndLogin/SignupAndLogin.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import './SignupAndLogin.css';
 
-import { FaUser, FaLock } from "react-icons/fa";
+import { FaUser, FaLock, FaEnvelope, FaPhone } from "react-icons/fa";
+import { FaHouse } from "react-icons/fa6";
 
 const SignupAndLogin = () => {
     return (
@@ -33,6 +34,53 @@ const SignupAndLogin = () => {
                         <p>
                             Don't have an account ?
                             <a href="#">Register</a>
+                        </p>
+                    </div>
+
+                </form>
+            </div>
+
+            <div className="form-box signup">
+                <form action="">
+                    <h1>Signup</h1>
+                    <div className="input-box">
+                        <input type="text"
+                        placeholder="Username" required />
+                        <FaUser className="icon"/>
+                    </div>
+                    <div className="input-box">
+                        <input type="email"
+                        placeholder="Email" required />
+                        <FaEnvelope className="icon"/>
+                    </div>
+                    <div className="input-box">
+                        <input type="tel"
+                        placeholder="Telephone" required />
+                        <FaPhone className="icon"/>
+                    </div>
+                    <div className="input-box">
+                        <input type="text"
+                        placeholder="Adress" required />
+                        <FaHouse className="icon"/>
+                    </div>
+                    <div className="input-box">
+                        <input type="password"
+                        placeholder="Password" required />
+                        <FaLock className="icon"/>
+                    </div>
+
+                    <div className="remember-forgot">
+                        <label>
+                            <input type="checkbox" /> I agree to the terms and conditions
+                        </label>
+                    </div>
+
+                    <button type="submit">Signup</button>
+
+                    <div className="register-link">
+                        <p>
+                            Already have an account ?
+                            <a href="#">Login</a>
                         </p>
                     </div>
 
