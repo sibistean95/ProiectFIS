ed9248b4f1448742c90f693e684c0d612b45ff62
Sibistean Adrian <adrian.sibistean@student.upt.ro>
2024-05-25 23:43:44 +0300

    Backend : Connection backend with database : Endpoints to add a new location

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 import express from "express"
 import mysql from "mysql"
+import multer from "multer";
 
 const app = express()
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage});
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -31,6 +35,39 @@ app.post("/user", (req, res)=> {
         return res.json(data);
     })
 })
+
+// endoint pentru a adauga o noua locatie
+app.post('/listing', upload.single('photo'), (req, res) => {
+    const { name, description, location, price_night, stars_number } = req.body;
+
+    if(!name || !location || !price_night || !stars_number) {
+        return res.status(400).json({ error: "All fields are required." });
+    }
+
+    const q = "INSERT INTO listing (name, description, location, price_night, stars_number, photo_url) VALUES (?, ?, ?, ?, ?, ?)";
+
+    db.query(q, [name, description, location, price_night, stars_number, photo_url], (err, result) => {
+        if (err) {
+            console.error("Error adding location:", err);
+            return res.status(500).json({ error: "Error adding location." });
+        }
+        
+        return res.status(201).json({ message: "Location added successfully." });
+    });
+})
+
+// endpoint pentru a obtine toate locatiile
+app.get('/listing', (req, res) => {
+    const query = "SELECT * FROM listing";
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error("Error getting listing:", err);
+            return res.status(500).json({ error: "Error getting listing." });
+        }
+        
+        return res.json(results);
+    });
+});
 
 app.listen(8800, ()=>{
     console.log("Connected to backend!")
